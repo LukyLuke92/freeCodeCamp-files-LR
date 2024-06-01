af21cb8b97fc4874bc5bb71a202a05764c941197
LukyLuke92 <lukerhym@gmail.com>
2024-06-01 08:45:47 -0400

    fix: added comments to clarify code

--- BackEndDevelopment/fileMetadata/index.js
+++ BackEndDevelopment/fileMetadata/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var cors = require('cors');
+// two lines below are syntax for using multer in the code below
+// note that if option dest is not included, the file will not be uploaded
+// (for this purpose, excluding it may be fine, but is included here anyway)
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' })
 require('dotenv').config()
@@ -13,7 +16,11 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// Post handler that returns file details
+// Note multer syntax expects arg of multer.single() to be name of the file input
+// (this is the input element with type "file" in index.html)
 app.post('/api/fileanalyse', upload.single('upfile'), (req,res) => {
+  // respond with json - req.file contains the uploaded file
   res.json({
     name: req.file.originalname,
     type: req.file.mimetype,
